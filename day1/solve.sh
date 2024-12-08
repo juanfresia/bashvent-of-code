200ed3b4fb9261f68f56cf6667e6350fcd5bdaa0
Juan Manuel fresia <juanmanuelfresia@gmail.com>
2024-12-07 23:15:12 -0300

    Day one with comments
    Signed-off-by: Juan Manuel fresia <juanmanuelfresia@gmail.com>

--- day1/solve.sh
+++ day1/solve.sh
@@ -15,15 +15,25 @@ mkdir ${TMP_DIR}
 cat ${INPUT} | awk '{print $1}' > ${TMP_DIR}/first_col
 cat ${INPUT} | awk '{print $2}' > ${TMP_DIR}/second_col
 
+# Sort each list separately
 sort ${TMP_DIR}/first_col > ${TMP_DIR}/first_sorted
 sort ${TMP_DIR}/second_col > ${TMP_DIR}/second_sorted
 
+# Combine the two sorted lists back into a single list, adding a `-` as a delimiter.
+# This will essentially result in a file that has a bunc of substractions (assuming no neg numbers on the original input)
 paste -d- ${TMP_DIR}/first_sorted ${TMP_DIR}/second_sorted > ${TMP_DIR}/combined
 
+# This is when we do the math, for each line of our substratcion list...
 for LINE in $(cat ${TMP_DIR}/combined); do
+	# Do a bash arithmetic evaluation $(( ... )) of the line
+	# The result could be negative still, since we don't know which list has the higher number,
+	# so take the absolute value by "dropping" the - sign with sed
 	echo $(($LINE)) | sed -e 's/-//'
 done > ${TMP_DIR}/diff
 
+# We do this to get rid of the last \n
 echo -n 0 >> ${TMP_DIR}/diff
 
+# We are here with the list of differences between each line
+# Replace all the \n with + signs and parse it all in a big arithmetic expression, et voilà
 echo $(( $(cat ${TMP_DIR}/diff | tr '\n' + ) ))
--- day1/solve_2.sh
+++ day1/solve_2.sh
@@ -15,15 +15,18 @@ mkdir ${TMP_DIR}
 cat ${INPUT} | awk '{print $1}' > ${TMP_DIR}/first_col
 cat ${INPUT} | awk '{print $2}' > ${TMP_DIR}/second_col
 
-sort ${TMP_DIR}/first_col > ${TMP_DIR}/first_sorted
-sort ${TMP_DIR}/second_col > ${TMP_DIR}/second_sorted
-
-for LINE in $(cat ${TMP_DIR}/first_sorted); do
-	COUNT=$(grep -E -c "^$LINE\$" ${TMP_DIR}/second_sorted || true)
+# For each number on our left list...
+for LINE in $(cat ${TMP_DIR}/first_col); do
+	# Run a beautiful regex matching exact lines (^...$) on the right list
+	# This is to prevent "342" from matching with "143425"
+	COUNT=$(grep -E -c "^$LINE\$" ${TMP_DIR}/second_col || true)
 	
+	# Bash arithmetic expression multiplying the two
 	echo $(($LINE * $COUNT))
 done > ${TMP_DIR}/diff
 
+# We do this to get rid of the last \n
 echo -n 0 >> ${TMP_DIR}/diff
 
+# Replace all the \n with + signs and parse it all in a big arithmetic expression, et voilà
 echo $(( $(cat ${TMP_DIR}/diff | tr '\n' + ) ))
