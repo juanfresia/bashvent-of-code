22e3336d6b4d60827435c1b6ad8dd14be2243a19
Juan Manuel fresia <juanmanuelfresia@gmail.com>
2024-12-08 00:21:49 -0300

    Day two, with comments
    Signed-off-by: Juan Manuel fresia <juanmanuelfresia@gmail.com>

--- day2/solve.sh
+++ day2/solve.sh
@@ -17,16 +17,27 @@ safe_counter="0"
 while IFS="" read -r LINE ; do
 	PREV_NUMBER=""
 	PREV_SIGN=""
+	# Check each line. Assume it is good, and skip it when the first error is found.
 	for NUMBER in $LINE ; do
+		# First iteration we just skip. We remember the number we saw
 		if [[ $PREV_NUMBER == "" ]]; then PREV_NUMBER=$NUMBER; continue; fi
+
+		# Calculate the difference and record the sign
 		DIFF=$(($PREV_NUMBER - $NUMBER))
 		if [[ "$DIFF" =~ "-" ]]; then SIGN="-"; else SIGN="+"; fi
+
+		# If this is the first difference we do, remember the sign
 		if [[ $PREV_SIGN == "" ]]; then PREV_SIGN=$SIGN; fi
+
+		# A difference in the sign, or a difference other than ±1, ±2 or ±3 immediatly breaks the loop.
+		# The "continue 2" here breaks the for loop _and_ sends us to the next iteration of the while loop.
+		# This way we skip  the increment of safe_counter variable
 		if [[ ! $PREV_SIGN == $SIGN ]]; then continue 2; fi
 		if [[ ! "$DIFF" =~ ^-?[123]$ ]]; then continue 2; fi
 		PREV_NUMBER=$NUMBER
 		PREV_SIGN=$SIGN
 	done
+	# If we reach this bit of code, the sequence is safe!
 	safe_counter=$(($safe_counter + 1))
 done < $INPUT
 
--- day2/solve_2.sh
+++ day2/solve_2.sh
@@ -15,9 +15,17 @@ safe_counter="0"
 
 # Split by line
 while IFS="" read -r LINE ; do
+	# Get the length of the line. We will be checking the line itself and all the sequences that are obtained
+	# by removing a single number from it.
 	LEN=$(echo $LINE | wc -w)
+	# This is crucial, we'll be abusing the fact that "cut" allows you to specify a field that does not exist. We count in a decreasing fashion
+	# so that the very first thing we do is to check the entire line. So, if the line has 5 numbers, well do 6 5 4 3 2 1...
 	for i in $(seq $(($LEN + 1)) -1 1) ; do
+		# The number i is the "field to skip". If it is greater than the number of fields in the line, nothing will be skipped.
+		# Normally "cut -fn" will only show field number n, but by using --complement we get _all fields but n_.
 		TEST_LINE=$(echo $LINE | cut --complement -f$i -d" ")
+
+		# Now it's the same logic to check the line.
 		PREV_NUMBER=""
 		PREV_SIGN=""
 		for NUMBER in $TEST_LINE ; do
@@ -25,15 +33,17 @@ while IFS="" read -r LINE ; do
 			DIFF=$(($PREV_NUMBER - $NUMBER))
 			if [[ "$DIFF" =~ "-" ]]; then SIGN="-"; else SIGN="+"; fi
 			if [[ $PREV_SIGN == "" ]]; then PREV_SIGN=$SIGN; fi
+
+			# This "continue 2" will skip two for loops before, that is, we'll generate a new TEST_LINE.
 			if [[ ! $PREV_SIGN == $SIGN ]]; then continue 2; fi
 			if [[ ! "$DIFF" =~ ^-?[123]$ ]]; then continue 2; fi
 			PREV_NUMBER=$NUMBER
 			PREV_SIGN=$SIGN
 		done
+		# The first time we see _any_ sequence succeed for a LINE, we mark it as safe and skip to the main while
 		safe_counter=$(($safe_counter + 1))
 		continue 2
 	done
-
 done < $INPUT
 
 echo $safe_counter
